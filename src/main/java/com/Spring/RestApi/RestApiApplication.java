84610f58eddfc0864a32443a70fc81ef2dad4474
Prajwal-banakar <prajwal.banakara@gmail.com>
2025-11-08 19:51:38 +0530

    Initial commit

---
+++ src/main/java/com/Spring/RestApi/RestApiApplication.java
@@ -0,0 +1,14 @@
+package com.Spring.RestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiApplication.class, args);
+        System.out.println("Application started");
+	}
+
+}
