9117846d3687799a9a29c0d5d6d7c582f2d1b027
Prajwal-banakar <prajwal.banakara@gmail.com>
2025-12-10 13:33:25 +0530

    modified

--- src/main/java/com/Spring/springbootapi/RestApiApplication.java
+++ src/main/java/com/Spring/springbootapi/RestApiApplication.java
@@ -1,4 +1,4 @@
-package com.Spring.RestApi;
+package com.Spring.springbootapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
